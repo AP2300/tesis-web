c68a22e11bd60e904bdd786b952fe84bf1e6b7ec
Jesus Romero <jesusenrique150620@gmail.com>
2021-03-27 17:03:46 -0400

    Media Query

---
+++ src/styles/sizes.js
@@ -0,0 +1,23 @@
+export default {
+    up() {},
+    Wdown(size) {
+      const sizes = {
+        xs: "575.98px",
+        sm: "767.98px",
+        md: "991.98px",
+        lg: "1199.98px",
+        xl: "1600px"
+      };
+      return `@media (max-width: ${sizes[size]})`;
+    },
+    Hdown(size) {
+        const sizes = {
+          xs: "575.98px",
+          sm: "767.98px",
+          md: "991.98px",
+          lg: "1199.98px",
+          xl: "1600px"
+        };
+        return `@media (max-height: ${sizes[size]})`;
+      }
+};
