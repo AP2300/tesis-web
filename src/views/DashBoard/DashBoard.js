aee9ce25aa55b28fcb2390def49e13e150a03774
Sebastian Montiel <Smontieltorres@gmail.com>
2021-04-21 17:10:43 -0400

    Refactorizacion codigo de History

--- src/views/DashBoard/DashBoard.js
+++ src/views/DashBoard/DashBoard.js
@@ -6,6 +6,7 @@ import useStyles from "../../styles/DashBoard";
 import clsx from 'clsx';
 import { Typography } from '@material-ui/core';
 import { GetGraphData } from '../../api/user';
+import { generateGraphData } from '../../helpers/Graph'
 var _ = require('lodash');
 var moment = require('moment');
 moment().format();
@@ -48,29 +49,13 @@ export default function DashBoard() {
     }, 200)
   }
 
-  function generateGraphData() {
-    let graph = [ 0 , 0 , 0 , 0 , 0 , 0 , 0 ];
-    const days = [1, 2, 3, 4, 5, 6, 0];
-    if(graphData) {
-      graphData[1].forEach(date => {
-          days.forEach((day,d) => {
-              if(day === moment(date.RegDate).day()){
-                  graph[d] += 1;                                 
-              }
-          });
-      });
-    }
-    return graph
-  }
-
   function getWeekAccess() {
     let calc = 0;
-    generateGraphData().forEach(e => {
+    generateGraphData(graphData).forEach(e => {
       calc += e;
     })
     return calc
   }
-
 
   return (
     <div className={classes.root}>
@@ -89,7 +74,7 @@ export default function DashBoard() {
               labels: ["Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado", "Domingo"],
               datasets: [{
                 label: "accesos",
-                data: generateGraphData(),
+                data: generateGraphData(graphData),
                 backgroundColor: "#f5deb382",
                 borderColor: "wheat"
               },]
--- src/views/History/History.js
+++ src/views/History/History.js
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import {
-    Accordion, AccordionSummary, AccordionDetails, FormHelperText,
+    Accordion, AccordionSummary, AccordionDetails,
     FormControlLabel, Typography, TextField, MenuItem, FormControl,
-    Paper, Divider, AccordionActions, Button, InputLabel, Select
+    Paper, Divider, AccordionActions, Button, Select
 } from "@material-ui/core";
 import { ExpandMore, FilterList } from "@material-ui/icons";
 import useStyles from "../../styles/History";
@@ -11,7 +11,7 @@ import clsx from 'clsx';
 import { Line } from "react-chartjs-2";
 import { useHistory } from 'react-router';
 import TitleContainer from '../../components/TitleContainer';
-const _ = require('lodash');
+import { setNumWeek, FilterSearch, ChangeGraph, calcNumWeek } from '../../helpers/Graph';
 const moment = require('moment');
 moment().format();
 
@@ -38,7 +38,7 @@ export default function History() {
     }, [Users])
 
     useEffect(() => {
-        if (week === "") setNumWeek();
+        if (week === "") NumWeek(year,month);
     }, [week])
 
     useEffect(() => {
@@ -46,7 +46,7 @@ export default function History() {
     }, [Textfield])
 
     useEffect(() => {
-        if (UserData != "") FilterSearch()
+        if (UserData != "") handleFilterSearch()
     }, [UserData])
 
 
@@ -56,6 +56,8 @@ export default function History() {
             setSearchData(res.data.data);
             setUsers(res.data.data);
             setIsPromiseReady(true);
+        } else {
+            history.push("/");
         }
     }
 
@@ -79,28 +81,12 @@ export default function History() {
         setExpanded(isExpanded ? panel : false);
         setIsUserPromiseReady(false);
     };
-
-    function calcNumWeek(){
-        let indexWeek = [];
-        let j = 0;
-        for (let i = 0; i < 5; i++) {
-            let date = String(moment(`${year}-${month+1}-${i === 0 ? 1 : (i*7)+j}`, "YYYY-MM-DD").startOf('isoWeek'));
-            if(i !== 0){
-                if(date === indexWeek[i-1]){
-                    j++;
-                    date = String(moment(`${year}-${month+1}-${i === 0 ? 1 : (i*7)+j}`, "YYYY-MM-DD").startOf('isoWeek'))
-                }
-            }
-            indexWeek.push(date);
-        }
-        return indexWeek;
-    }
-
-    function setNumWeek(){
-        let indexWeek = calcNumWeek();
+    
+    function NumWeek(year,month){
+        let indexWeek = calcNumWeek(year,month);
         indexWeek.forEach((e,i) => {
             if(e === String(moment(moment()._d, "DD MM YYYY hh:mm:ss").startOf('isoWeek'))){
-                setWeek(i);
+                setWeek(e); 
             }
         });
     }
@@ -122,97 +108,9 @@ export default function History() {
         });
     }
 
-    async function FilterSearch() {
-            let graphData = [];
-            let groupedResults = "";
-            let result = "";
-            switch (TimeStamp) {
-                case "S":
-                    groupedResults = _.groupBy(UserData, (UserData) => moment(UserData.RegDate).startOf('month'));
-                    result = Object.entries(groupedResults);
-                    result.forEach((e) => {
-                        if(month === moment(e[0]).month() && year === moment(e[0]).year()){
-                            let dates = _.groupBy(e[1], (DateData) => moment(DateData.RegDate).startOf('isoWeek'));
-                            let order = Object.entries(dates);
-                            graphData = _.orderBy(order, (DateData) => moment(DateData[0]).startOf('isoWeek'));
-                        }
-                    })
-                    ChangeGraph(graphData);
-                break;
-                case "M":
-                    groupedResults = _.groupBy(UserData, (UserData) => moment(UserData.RegDate).startOf('month'));
-                    result = Object.entries(groupedResults);
-                    result.forEach((e) => {
-                        if(month === moment(e[0]).month() && year === moment(e[0]).year()){
-                            let dates = _.groupBy(e[1], (DateData) => moment(DateData.RegDate).startOf('isoWeek'));
-                            let order = Object.entries(dates);
-                            graphData = _.orderBy(order, (DateData) => moment(DateData[0]).startOf('isoWeek'));
-                        }
-                    })
-                    ChangeGraph(graphData);
-                break;
-                case "A":
-                    groupedResults = _.groupBy(UserData, (UserData) => moment(UserData.RegDate).startOf('year'));
-                    result = Object.entries(groupedResults);
-                    result.forEach((e) => {
-                        if(year === moment(e[0]).year()){
-                            // ordenar por Mes 
-                            let monthsData = _.groupBy(e[1], (Data) => moment(Data.RegDate).startOf('month'));
-                            let res = Object.entries(monthsData);
-                            graphData = _.orderBy(res, (Data) => moment(Data[0]).startOf('month'));
-                        }
-                    })
-                    ChangeGraph(graphData);
-                break;
-            }
+    async function handleFilterSearch(){
+        setGraph(ChangeGraph(TimeStamp,year,month,week,FilterSearch(UserData, month, year, TimeStamp)));
     }
-
-    async function ChangeGraph(graphData){
-        let graphW = [ 0 , 0 , 0 , 0 , 0 , 0 , 0 ];
-        let graphM = [ 0 , 0 , 0 , 0 , 0 ];
-        let graphY = [ 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 ];
-        let indexWeek = calcNumWeek();
-        switch (TimeStamp) {
-            case "S":
-                const days = [1, 2, 3, 4, 5, 6, 0];
-                if (graphData) {
-                    graphData[week][1].forEach(date => {
-                        days.forEach((day,d) => {
-                            if(day === moment(date.RegDate).day()){
-                                graphW[d] += 1;                                 
-                            }
-                        });
-                    });
-                    setGraph([["Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado", "Domingo"],graphW])
-                }
-            break;
-            case "M":
-                const weeks = ["1era", "2da", "3era", "4ta", "5ta"];
-                if(graphData){
-                    graphData.forEach((d,i) => {
-                        indexWeek.forEach((w) =>{
-                            if(d[0] === w){
-                                graphM[i] = d[1].length;
-                            }
-                        })
-                    })
-                    setGraph([weeks,graphM])
-                }
-            break;
-            case "A":
-                const monthsShort = moment()._locale._monthsShort;
-                const months = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
-                if(graphData){
-                    for (let i = 0; i < 12; i++) {
-                        graphData.forEach((y) => {
-                            if(monthsShort[i] === y[0].split(" ")[1]) graphY[i] = y[1].length; 
-                        })
-                    }
-                    setGraph([months,graphY])
-                }
-            break;
-        }
-    } 
 
     return (
         <div >
@@ -336,9 +234,8 @@ export default function History() {
                                                         }
                                                     }
                                                     }
-                                                /> : ""
+                                                /> : "" 
                                             }
-
                                         </div>
                                         <div className={clsx(classes.column, isUserPromiseReady ? "" : classes.loading, classes.helper)}>
 
@@ -352,7 +249,6 @@ export default function History() {
                   </Button>
                                     </AccordionActions>
                                 </Accordion>
-
                             )
                         }) : ""
                 }
