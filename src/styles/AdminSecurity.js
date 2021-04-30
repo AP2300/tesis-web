3735312c7c071634630458a6bbcb30a48c5094b8
Jesus Romero <jesusenrique150620@gmail.com>
2021-04-30 13:52:52 -0400

    Progress on admin security

---
+++ src/styles/AdminSecurity.js
@@ -0,0 +1,238 @@
+import { makeStyles } from '@material-ui/core/styles';
+import sizes from "./sizes";
+
+let useStyles = "";
+
+export default useStyles = makeStyles((theme) => ({
+    root: {
+        display: 'flex',
+        flexWrap: 'wrap',
+        marginLeft: "2%",
+        marginRight: "2%",
+        "& > *": {
+            color: "#5f5f5f"
+        }
+    },
+    mainContainer: {
+        width: "100%",
+        height: "80vh"
+    },
+    panelContainer: {
+        height: "80vh",
+        padding: "2%",
+        display: "flex",
+        justifyContent: "space-between",
+        [sizes.Wdown("xs")]: {
+            flexDirection: "column",
+        },
+    },
+    minimizerButton: {
+        width: "5%",
+        color: "inherit",
+        [sizes.Wdown("xs")]: {
+            height: "5%",
+            width: "100%",
+            "& .icon":{
+                transform: "rotate(90deg)"
+            }
+        },
+    },
+    maximizedContainerUsers: {
+        width: "30%",
+        height: "100%",
+        transition: "250ms ease-in",
+        color: "inherit",
+        [sizes.Wdown("xs")]: {
+            height: "85%",
+            width: "100%"
+        },
+        
+    },
+    maximizedContainerSecurity: {
+        width: "85%",
+        height: "100%",
+        transition: "250ms ease-in",
+        color: "inherit",
+        [sizes.Wdown("xs")]: {
+            height: "85%",
+            width: "100%"
+        },
+        
+    },
+    minimizedContainerUsers: {
+        width: "8%",
+        height: "100%",
+        transition: "250ms ease-in",
+        color: "inherit",
+        [sizes.Wdown("xs")]: {
+            height: "8%",
+            width: "100%"
+        },
+    },
+    minimizedContainerSecurity: {
+        width: "63%",
+        height: "100%",
+        transition: "250ms ease-in",
+        color: "inherit",
+        [sizes.Wdown("xs")]: {
+            height: "8%",
+            width: "100%"
+        },
+    },
+    logo: {
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    dataContainer: {
+        display: "flex",
+        justifyContent: "flex-start",
+        alignItems: "flex-start",
+        padding: "2em 0em 2em 2em",
+    },
+    dataContainerSec: {
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "stretch",
+        height: "100%"
+    },
+    List: {
+        display: "flex",
+        flexDirection: "column",
+        height: "55vh",
+        overflowY: "scroll", 
+        alignItems: "flex-start",
+        paddingRight: "1em"
+    },
+    ListItem: {
+        width: "15em",
+        padding: "0.5rem 1rem",
+        marginTop: "0.5rem",
+        color: "#000000a6",
+        display: "flex",
+        flexDirection: "row",
+        borderRadius: "1rem",
+        fontSize: "2.2vmin"
+    },
+    listItemText: {
+        fontWeight: "600",
+        fontSize: `calc(18px + (30 - 18) * ((40vw - 320px) / (1600 - 300)))`
+    },
+    active: {
+        backgroundColor: "#0000001a"
+    },
+    motherContainer: {
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-evenly",
+        alignItems: "stretch",
+        height: "83%"
+    },
+    facialContainer: {
+        color: "inherit",
+        width: "50%",
+        margin: "1rem",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "space-around",
+        padding: "0rem 1rem",
+    },
+    faceAvatar: {
+        width: "9rem",
+        height: "9rem"
+    },
+    fingerContainer: {
+        color: "inherit",
+        width: "50%",
+        margin: "1rem",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "space-around",
+        padding: "0rem 1rem",
+    },
+    fingerAvatar: {
+        width: "7.5rem",
+        height: "7.5rem"
+    },
+    AuthItem: {
+        margin: "1.3%",
+        display: "flex",
+        alignItems: "center",
+        width: "70%",
+        cursor: "default",
+        height: "6.7vh",
+        "& > .AuthName": {
+            width: "45%",
+            height: "100%",
+            display: "flex",
+            alignItems: "center",
+            cursor: "pointer",
+            justifyContent: "center",
+            zIndex: "12",
+            transition: "350ms",
+            "& > * ": {
+                fontWeight: 600,
+                color: "#5f5f5f"
+            }
+        },
+        "& > .AuthNameDis": {
+            width: "45%",
+            height: "100%",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "space-evenly",
+            zIndex: "12",
+            backgroundColor: "#c4c4c4",
+            transition: "350ms",
+            "& > * ": {
+                fontWeight: 600,
+                color: "#8a8a8a"
+            }
+        },
+        "& > .IsActive": {
+            width: "55%",
+            height: "100%",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            color: "whitesmoke",
+            transition: "350ms",
+        },
+        "& > .IsDis": {
+            width: "55%",
+            height: "100%",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            color: "#636363",
+            transition: "350ms",
+            fontSize: "1em",
+        },
+        "& > * ": {
+            fontWeight: 600,
+            color: "#5f5f5f"
+        }
+    },
+    disabled:{  
+        "& > .AuthName":{
+            transform: "translateX(122.5%)",
+            transition: "350ms"
+        },
+        "& > .IsActive":{
+            transform: "translateX(-80%)",
+            transition: "350ms"
+        }
+    },
+    red: {
+        backgroundColor: "#f44336"
+    },
+    green: {
+        backgroundColor: "#4caf50"
+    },
+    button: {
+        textTransform: "none",
+        color: "inherit"
+    },
+}));
--- src/views/AdminSecurity/AdminSecurity.js
+++ src/views/AdminSecurity/AdminSecurity.js
@@ -1,6 +1,212 @@
+import React, { useState, useEffect } from 'react';
+import Paper from '@material-ui/core/Paper';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
+import List from '@material-ui/core/List';
+import Avatar from '@material-ui/core/Avatar';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import useStyles from '../../styles/AdminSecurity';
+import PeopleIcon from '@material-ui/icons/People';
+import Fingerprint from '@material-ui/icons/Fingerprint';
+import Mood from '@material-ui/icons/Mood';
+import SecurityIcon from '@material-ui/icons/Security';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import clsx from 'clsx';
+import { GetHistoryData, GetSecurityUserData, UpdateAuthMethods } from '../../api/user';
+import { useHistory } from 'react-router-dom';
 
 export default function AdminSecurity() {
+    const history = useHistory();
+    const [animations, setAnimations] = useState({ Minimize: false });
+    const [activeUser, setActiveUser] = useState("");
+    const [userData, setUserData] = useState([{isActive: true, name: "Facial"}, {isActive: true, name: "Huella"}])
+    const [userList, setUserList] = useState([])
+    const [isPromiseReady, setIsPromiseReady] = useState(false);
+    const [isSecDataReady, setIsSecDataReady] = useState(false);
+    const classes = useStyles();
+
+    useEffect(async () => {
+        if(userList.length == 0) {
+            console.log(userList)
+            let data = await GetHistoryData();
+            if(data) {
+                setUserList(data.data.data);
+                console.log(userList);
+                setIsPromiseReady(true);
+                console.log(isPromiseReady);
+            }
+        } else {
+            console.log("false")
+        }
+    }, [userList])
+
+    function handleMinimize() {
+        if (!animations.Minimize) setAnimations({ Minimize: true })
+        else setAnimations({ Minimize: false })
+    }
+
+    function Toggle(name) {
+        let Update = JSON.parse(JSON.stringify(userData))
+        console.log(Update);
+        Update[name].IsActive = Number(!Update[name].IsActive)
+        const IsToUpdate = Update[name].some(el => el.IsActive && el.Name != "Codigo")
+        if (IsToUpdate) {
+            const params = {
+                id: Update[name].IDSecurity,
+                active: Update[name].IsActive
+            }
+            const res = UpdateAuthMethods(params)
+            if (!res) history.push("/")
+            else setUserData(Update)
+        } else {
+            console.log("asdasdasd")
+            /*setNoti({
+                ...noti, severity: "warning",
+                description: "No puedes desactivar tu último metodo de autenticacion activo",
+                open: true
+            })*/
+
+        }
+    }
+
+    async function fetchUserSecurity(name, id) {
+        console.log(id);
+        console.log(name)
+        let data = await GetSecurityUserData(id);
+        let d = ""
+        if(data) {
+            if(data.length!==0) {
+                console.log(data.data.data);
+                const codigo = data.data.data.filter((d) => d.Name == "Codigo")
+                const huella = data.data.data.filter((d) => d.Name == "Huella")
+                const facial = data.data.data.filter((d) => d.Name == "Facial")
+                d = {codigo, huella, facial}
+                console.log(d);
+            } else {
+                d = {}
+            }
+            setActiveUser(name);
+            setUserData(d);
+            setIsSecDataReady(true);
+        }
+    }
+
     return(
-        <div>Admin Security</div>
+        <div className={classes.root}>
+            <Paper className={classes.mainContainer}>
+                <div className={classes.panelContainer}>
+                    <Paper className={clsx(!animations.Minimize ? [classes.maximizedContainerUsers, classes.dataContainer] : [classes.minimizedContainerUsers, classes.logo])} elevation={2}>
+                        {!animations.Minimize ? (
+                            <div style={{width: "100%", height: "100%"}}>
+                                <Typography gutterBottom variant="h4">
+                                    Usuarios:
+                                </Typography>
+                                <List className={classes.List}>
+                                    {isPromiseReady ? userList.map((data, index) => {
+                                        return (
+                                            <ListItem button key={index} onClick={() => {fetchUserSecurity(data.FullName, data.IDUser)}} className={clsx(classes.ListItem, activeUser === data.FullName ? classes.active : "")}>
+                                                <ListItemText primary={data.FullName} classes={{ primary: classes.listItemText }}/>
+                                            </ListItem>
+                                        )
+                                    }) : "cargando"} 
+                                </List>
+                            </div>
+                        )
+                        : <PeopleIcon fontSize="large"/>}
+                    </Paper>
+                    <Button className={classes.minimizerButton} onClick={handleMinimize}>
+                        {animations.Minimize ? <ChevronRightIcon className="icon" /> : <ChevronLeftIcon className="icon" />}
+                    </Button>
+                    <Paper className={clsx(animations.Minimize ? classes.maximizedContainerSecurity : classes.minimizedContainerSecurity)}>
+                    {(isSecDataReady && Object.keys(userData).length!=0) ? (
+                        <div className={classes.dataContainerSec}>
+                            <div style={{display: "flex", justifyContent: "center", flexDirection: "column", alignItems: "center", padding: "1em 0em 0em 0em",}}>
+                                <Typography variant="h4" style={{fontSize: "calc(1em + 1vw)" }}>
+                                    Usuario: {activeUser}
+                                </Typography>
+                                <Typography gutterBottom variant="h6" style={{fontSize: "calc(0.7em + 0.7vw)" }}>
+                                    Código de acceso: {userData.codigo.data}
+                                </Typography>
+                                <Divider orientation="horizontal" variant={"middle"} style={{width: "95%"}} />
+                            </div>
+                        
+                        <div className={classes.motherContainer}>
+                        
+                            <Paper className={classes.facialContainer}>
+                                <Typography variant="h5" style={{fontSize: "calc(0.8em + 0.8vw)", margin: "0.3em 0"}}>
+                                    Foto para reconocimiento facial
+                                </Typography>
+                                <Avatar className={classes.faceAvatar} src={`http://localhost:3001${userData.facial.data}`}/>
+                                <div style={{display: "flex", flexDirection: "row", alignItems: "center", justifyContent: "space-evenly", width: "100%"}}>
+                                    <Button variant="outlined" className={classes.button}>
+                                        Editar cara
+                                    </Button>
+                                    <Button variant="outlined" className={classes.button}>
+                                        Eliminar cara
+                                    </Button>
+                                </div>
+                                <Paper onClick={() => Toggle(userData.facial.Name)} key={userData.facial.Name}
+                                className={clsx(classes.AuthItem, userData.facial.IsActive && classes.disabled, userData.facial.IsActive ? classes.green : classes.red)} elevation={1}>
+                                    <Paper className="AuthName" elevation={0}>
+                                        <Mood />
+                                        <Typography>
+                                            {userData.facial.Name}
+                                        </Typography>
+                                    </Paper>
+                                    <Typography className="IsActive">
+                                        {userData.facial.IsActive ? "Activo" : "Inactivo"}
+                                    </Typography>
+                                </Paper>
+                            </Paper>
+                            <Paper className={classes.fingerContainer}>
+                                <Typography variant="h5" style={{fontSize: "calc(0.8em + 0.8vw)", margin: "0.3em 0"}}>
+                                    Huellas dactilares
+                                </Typography>
+                                <Divider orientation="horizontal" variant={"middle"} style={{width: "90%"}}/>
+                                <div style={{overflowY: "scroll", width: "100%"}}>
+                                    {userData.huella.map((data, index) => {
+                                        return (
+                                            <div key={data.IDBiometrics} style={{display: "flex", alignItems: "center", justifyContent: "center", flexDirection: "column", margin: "0.5em 0"}}>
+                                                <Avatar className={classes.fingerAvatar} style={{margin: "0.4em 0"}}/>
+                                                <Button variant="outlined" className={classes.button} style={{margin: "0.3em 0"}}>
+                                                    Editar huella
+                                                </Button>
+                                                <Button variant="outlined" className={classes.button}>
+                                                    Eliminar huella
+                                                </Button>
+                                            </div>
+                                        )
+                                    })}
+                                </div>
+                                
+                                <div style={{width: "100%"}}>                                    
+                                    <Divider orientation="horizontal" variant={"middle"} />
+                                    <Paper onClick={() => Toggle(userData.huella.Name)} key={userData.huella.Name} className={clsx(classes.AuthItem, userData.huella.IsActive && classes.disabled, userData.huella.IsActive ? classes.green : classes.red)} elevation={1} style={{margin: "0.2em auto"}}>
+                                    <Paper className="AuthName" elevation={0}>
+                                        <Fingerprint />
+                                        <Typography>
+                                            {userData.huella.name}
+                                        </Typography>
+                                        </Paper>
+                                        <Typography className="IsActive">
+                                            {userData.huella.IsActive ? "Activo" : "Inactivo"}
+                                        </Typography>
+                                    </Paper>
+                                </div>
+                            </Paper>
+                        </div>
+                        </div>
+                    ) :
+                    <Typography>
+                        No hay datos para mostrar
+                    </Typography>
+                }
+                    </Paper>
+                </div>
+            </Paper>
+        </div>
     )
 }
